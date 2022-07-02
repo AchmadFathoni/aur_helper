2ce51004f19b77ea8f3f8595c34aab44791a3fed
Achmad Fathoni <fathoni.id@gmail.com>
2022-07-02 09:13:03 +0700

    exclude zoom

--- rebuild.bash
+++ rebuild.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 while read p; do
-    yay -S "$p" --rebuildall --noconfirm
-done <<< $(checkrebuild | cut -f2)
+    yay -S "$p" --rebuild --noconfirm
+done <<< $(checkrebuild | cut -f2 | grep -v 'zoom')
